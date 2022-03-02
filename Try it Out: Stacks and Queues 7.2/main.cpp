0c73f84fe3de3e66966904fa5a90711432b623a2
Coleton Watt <electricninja102@gmail.com>
2022-03-01 18:27:39 -0700

    Initial Commit

---
+++ Try it Out: Stacks and Queues 7.2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Try it Out: Stacks and Queues 7.2
+//
+//  Created by Coleton Watt on 3/1/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
